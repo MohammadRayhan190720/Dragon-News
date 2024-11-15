7536d9ad926beed1b01da716d864abc050dbe52a
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-11-15 18:49:09 +0600

    added navbar

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -1,12 +1,22 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayouts = () => {
   return (
-    <div>
-       <Header></Header>  
-       <LatestNews></LatestNews>
-  </div>
+    <div className="font-poppins">
+      <Header></Header>
+      <LatestNews></LatestNews>
+      <div className="w-11/12 mx-auto pt-5">
+        <Navbar></Navbar>
+        <div className="grid md:grid-cols-12 mt-5">
+          <div className="col-span-3 border">Left Nav</div>
+          <div className="col-span-6 border">Main COntetnt</div>
+          <div className="col-span-3 border">Right Nav</div>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
